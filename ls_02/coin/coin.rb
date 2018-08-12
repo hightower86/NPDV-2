f9a3aecf34289bbe054b6667f9408e27194a8368
Adel <htwr86@gmail.com>
2018-08-12 15:26:28 +0300

    add coin-game

---
+++ ls_02/coin/coin.rb
@@ -0,0 +1,11 @@
+# coin.rb
+# Сгенерим случайное число от 0 до 10 тем же методом  rand
+if rand(11) == 10
+  puts "Ребро"
+else
+  if rand(2) == 1
+    puts "Reshka"
+  else
+    puts "Orel"
+  end
+end
